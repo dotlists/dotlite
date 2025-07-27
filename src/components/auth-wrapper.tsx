551192998ce3e05ec79ce135b74b834d7b4c2442
StarLight842 <842starlite@gmail.com>
2025-07-26 23:34:43 -0700

    new design trust

--- src/components/auth-wrapper.tsx
+++ src/components/auth-wrapper.tsx
@@ -17,19 +17,23 @@ export default function AuthWrapper(props: { Content: React.ComponentType }) {
     <>
       <main>
         <Authenticated>
-          <div className="fixed w-full top-0 p-2 bg-white z-[1000]">
-            <div className="flex gap-2">
+          <div>
+            {/* Top left: user info */}
+            <div className="fixed top-2 left-2 flex items-center gap-2 bg-white p-2 rounded shadow z-[1000]">
               <img
                 src={image ?? undefined}
-                className="w-7 h-7 rounded-full my-auto"
-              ></img>
-              <p className="my-auto mr-auto">{viewer ?? "Anonymous"}</p>
-              {isAuthenticated && (
+                className="w-7 h-7 rounded-full"
+              />
+              <p>{viewer ?? "Anonymous"}</p>
+            </div>
+            {/* Bottom right: sign out button */}
+            {isAuthenticated && (
+              <div className="fixed top-2 right-2 z-[1000]">
                 <Button variant="fancy" onClick={() => void signOut()}>
                   Sign out
                 </Button>
-              )}
-            </div>
+              </div>
+            )}
           </div>
 
           <props.Content />
