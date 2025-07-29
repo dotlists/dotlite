64c4c633d35e86a0bed5787c4bfdac2e130fe96d
StarLight842 <842starlite@gmail.com>
2025-07-29 01:08:23 -0700

    feat: works as a PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { URL, fileURLToPath } from "url";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate", // Automatically update the PWA
+      includeAssets: [
+        'favicon.png', // or your favicon's full name
+        'Geist-P*.woff2', // Using a glob pattern for fonts
+        'GeistM*.woff2',
+        'pp-edit*.woff2'
+      ],
+      manifest: {
+        name: "Dotlist Lite",
+        short_name: "Dotlist",
+        description: "Dotlist Lite is a simple, fast, and polished task management app.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+      alias: {
+        // Update this alias
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
 });
